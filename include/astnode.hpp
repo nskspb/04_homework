45eb70a575a9f8747ae063d0dca93ddbc3db7309
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-02-19 21:32:05 +0300

    virtual ~ASTNode()

--- include/astnode.hpp
+++ include/astnode.hpp
@@ -7,30 +7,31 @@
 #include <iostream>
 #include <string>
 
-class ASTNode {
-  public:
-    // Конструктор листа (узел без дочерних узлов)
-    explicit ASTNode(const std::string &repr);
-    
-    // Конструктор узла (с одним или двумя дочерними узлами)
-    ASTNode(const std::string &repr, ASTNode *lhs, ASTNode *rhs);
+class ASTNode
+{
+public:
+  // Конструктор листа (узел без дочерних узлов)
+  explicit ASTNode(const std::string &repr);
 
-    ASTNode(const ASTNode &other) = delete;
+  // Конструктор узла (с одним или двумя дочерними узлами)
+  ASTNode(const std::string &repr, ASTNode *lhs, ASTNode *rhs);
 
-    ASTNode &operator=(const ASTNode &other) = delete;
+  ASTNode(const ASTNode &other) = delete;
 
-    ~ASTNode();
+  ASTNode &operator=(const ASTNode &other) = delete;
 
-    // Представления имени узла в виде строки
-    std::string repr() const { return repr_; }
+  virtual ~ASTNode();
 
-    // Печать всего дерева в текстовом виде
-    void print(std::ostream &out) const;
+  // Представления имени узла в виде строки
+  std::string repr() const { return repr_; }
 
-  private:
-    void inner_print(std::ostream &out, size_t indent) const;
+  // Печать всего дерева в текстовом виде
+  void print(std::ostream &out) const;
 
-    std::string repr_;
-    ASTNode *lhs_;
-    ASTNode *rhs_;
+private:
+  void inner_print(std::ostream &out, size_t indent) const;
+
+  std::string repr_;
+  ASTNode *lhs_;
+  ASTNode *rhs_;
 };
