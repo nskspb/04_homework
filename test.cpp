2464f43eb88e28f228f1dd02f60c7368cf0f5393
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-02-19 21:34:39 +0300

    version 2.0

--- test.cpp
+++ test.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <cstdlib>
+#include <iostream>
 
-int main() {
+using std::cout;
+using std::endl;
 
-    enum operations {
+class A
+{
+public:
+    A() { cout << "A()" << endl; }
+    ~A() { cout << "~A()" << endl; }
+};
+
+class B : public A
+{
+public:
+    B() { cout << "B()" << endl; }
+    ~B() { cout << "~B()" << endl; }
+};
+
+int main()
+{
+
+    B b;
+    return EXIT_SUCCESS;
+
+    enum operations
+    {
         add = 1,
         substract = 2,
         multiply = 3,
@@ -13,29 +37,30 @@ int main() {
     double x1, x2;
     double result;
 
-    std :: cout << "Add: 1 \tSubstract: 2 \tMultiply: 3 \tDivision: 4" << std::endl;
-    std :: cout << "Input x1: ";
-    std :: cin >> x1;
-    std :: cout << "Input x2: ";
-    std :: cin >> x2;
-    std :: cout << "Input operation number: ";
-    std :: cin >> operation;
+    std ::cout << "Add: 1 \tSubstract: 2 \tMultiply: 3 \tDivision: 4" << std::endl;
+    std ::cout << "Input x1: ";
+    std ::cin >> x1;
+    std ::cout << "Input x2: ";
+    std ::cin >> x2;
+    std ::cout << "Input operation number: ";
+    std ::cin >> operation;
 
-    switch (operation) {
-        case operations::add:
-            result = x1 + x2;
-            break;
-        case operations::substract:
-            result = x1 - x2;
-            break;
-        case operations::multiply:
-            result = x1 * x2;
-            break;
-        case operations::division:
-            result = x1 / x2;
-            break;
-            }
-    std :: cout << "Result: " << result << std::endl;
-    std :: cout << operations::division << std::endl;
+    switch (operation)
+    {
+    case operations::add:
+        result = x1 + x2;
+        break;
+    case operations::substract:
+        result = x1 - x2;
+        break;
+    case operations::multiply:
+        result = x1 * x2;
+        break;
+    case operations::division:
+        result = x1 / x2;
+        break;
+    }
+    std ::cout << "Result: " << result << std::endl;
+    std ::cout << operations::division << std::endl;
     return 0;
 }
